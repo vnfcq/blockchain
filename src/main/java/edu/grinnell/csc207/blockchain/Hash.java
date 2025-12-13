8a90e2c881c46107a8d29b073c5af896f796c885
vnfcq <seva.sulimov@gmail.com>
2025-12-12 22:06:35 -0600

    Implement the hash class

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,8 +1,45 @@
 package edu.grinnell.csc207.blockchain;
 
+import java.util.Arrays;
+
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
-    // TODO: fill me in!
+
+    private final byte[] data;
+
+    public Hash(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public boolean isValid() {
+        return data.length >= 3 && data[0] == 0 && data[1] == 0 && data[2] == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(data.length * 2);
+        for (byte b : data){
+            int unsignedInt = Byte.toUnsignedInt(b);
+            sb.append(String.format("%02x", unsignedInt));
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other){
+            return true;
+        }
+        if (!(other instanceof Hash)){
+            return false;
+        }
+        Hash o = (Hash) other;
+        return Arrays.equals(this.data, o.data);
+    }
 }
